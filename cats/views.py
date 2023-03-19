a72809c098e9fa488a615f94b6b21831e24607ac
Андрей Варачев <118448717+Dartanyun@users.noreply.github.com>
2023-03-19 23:21:28 +0500

    Progress in Y.Practicum

--- cats/views.py
+++ cats/views.py
@@ -1,14 +1,33 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from .models import Cat, Owner
-from .serializers import CatSerializer, OwnerSerializer
+from .serializers import CatSerializer, OwnerSerializer, CatListSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
 
+    def get_serializer_class(self):
+        # Если запрошенное действие (action) — получение списка объектов ('list')
+        if self.action == 'list':
+            # ...то применяем CatListSerializer
+            return CatListSerializer
+        # А если запрошенное действие — не 'list', применяем CatSerializer
+        return CatSerializer
+
 
 class OwnerViewSet(viewsets.ModelViewSet):
     queryset = Owner.objects.all()
     serializer_class = OwnerSerializer
+
+
+class CreateRetrieveViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                            viewsets.GenericViewSet):
+    # В теле класса никакой код не нужен! Пустячок, а приятно.
+    pass 
+
+
+class LightCatViewSet(CreateRetrieveViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer 
